d54050b81bb1dab4f904e9e0c26e160b4557b821
SmallsyBigs <bobionekenobi241@gmail.com>
2024-04-10 20:09:02 +0200

    add basic styling

--- javascript.js
+++ javascript.js
@@ -2,5 +2,6 @@ const gridContainer = document.querySelector(".gridContainer");
 
 for(let i = 0; i < 16; i++){
 const gridDiv = document.createElement("div");
+gridDiv.classList.add("gameGrid");
 gridContainer.appendChild(gridDiv);
 }
