752b4fa0a7d530449074af73112f22808742ad6f
SmallsyBigs <bobionekenobi241@gmail.com>
2024-04-10 19:33:38 +0200

    add script to create sketch grids

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const gridContainer = document.querySelector(".gridContainer");
+
+for(let i = 0; i < 16; i++){
+const gridDiv = document.createElement("div");
+gridContainer.appendChild(gridDiv);
+}
