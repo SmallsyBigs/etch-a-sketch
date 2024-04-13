79609c5c4d6cf54f9ef50c49d7a89eed8b9db0b0
SmallsyBigs <bobionekenobi241@gmail.com>
2024-04-13 09:55:45 +0200

    change event listener structure

--- javascript.js
+++ javascript.js
@@ -4,12 +4,14 @@ for(let i = 0; i < 256; i++){
 const gameGrid = document.createElement("div");
 gameGrid.classList.add("gameGrid");
 gridContainer.appendChild(gameGrid);
-gameGrid.onmouseenter = function(){
+
+gameGrid.addEventListener("mouseenter", function(){
     gameGrid.style.backgroundColor = "red";
-}
-gameGrid.onmouseout = function(){
+})
+
+gameGrid.addEventListener("mouseleave", function(){
     const newRandomColor = "#" + Math.floor(Math.random()*16777215).toString(16)
     gameGrid.style.backgroundColor = newRandomColor
-}
+    })
 }
 
