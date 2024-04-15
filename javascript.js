85a44848d7742eafe378e7e2fb17210a28126a22
SmallsyBigs <bobionekenobi241@gmail.com>
2024-04-15 17:03:45 +0200

    modify listener behaviour

--- javascript.js
+++ javascript.js
@@ -1,16 +1,7 @@
 const gridContainer = document.querySelector(".gridContainer");
 
-const newGridBtn = document.querySelector("#getSizeBtn");
-
-newSize = newGridBtn.addEventListener("click", function(){
-    newSize = prompt("Choose number per side (max:16)");
-    if(newSize > 0 && newSize <= 16) {
-    return newSize * newSize
-    }
-    else{
-        return alert("Wrong number :(");
-    }
-})
+// The purpuse of the listener is to provide the default grid size 
+// and basic functionalities
 
 for(let i = 0; i < 256; i++){
 const gameGrid = document.createElement("div");
@@ -19,10 +10,40 @@ gridContainer.appendChild(gameGrid);
 
 gameGrid.addEventListener("mouseenter", function(){
     gameGrid.style.backgroundColor = "red";
-})
+}, {once : true})
 
 gameGrid.addEventListener("mouseleave", function(){
     const newRandomColor = "#" + Math.floor(Math.random()*16777215).toString(16)
     gameGrid.style.backgroundColor = newRandomColor
-    })
+    }, {once : true})
 }
+
+
+const newGridBtn = document.querySelector("#getSizeBtn");
+
+// Code's purpuse is to provide choose size functionality
+// in order for the user to be ablo to dynamically change grid's size
+
+newGridBtn.addEventListener("click", function(){
+    newSize = prompt("Choose number per side (max:16)");
+    if(newSize > 0 && newSize <= 16) {
+    newSize = newSize * newSize
+    }
+    else{
+        return alert("Wrong number :(");
+    }
+    for(let i = 0; i < newSize; i++){
+        const gameGrid = document.createElement("div");
+        gameGrid.classList.add("gameGrid");
+        gridContainer.appendChild(gameGrid);
+        
+        gameGrid.addEventListener("mouseenter", function(){
+            gameGrid.style.backgroundColor = "red";
+        }, {once : true})
+        
+        gameGrid.addEventListener("mouseleave", function(){
+            const newRandomColor = "#" + Math.floor(Math.random()*16777215).toString(16)
+            gameGrid.style.backgroundColor = newRandomColor
+            }, {once : true})
+        }
+})
