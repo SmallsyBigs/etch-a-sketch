8f21551b23ddf47fd4fc5742a68a9594bfe324a1
SmallsyBigs <bobionekenobi241@gmail.com>
2024-04-18 19:07:56 +0200

    add basic functions

--- javascript.js
+++ javascript.js
@@ -1,8 +1,27 @@
 const gridContainer = document.querySelector(".gridContainer");
+const sizeBtn = document.querySelector("#getSizeBtn");
+const resetBtn = document.querySelector("#getResetBtn");
+let gridSize = 16;
 
 function makeGrid(){
-    for(let i = 0; i < 256; i++){
-        
+    for(let i = 0; i < gridSize * gridSize; i++){
+        gameGrid = document.createElement("div");
+        gameGrid.classList.add("gameGrid");
+        gridContainer.appendChild(gameGrid);
+        gameGrid.style.width = `calc(100% / ${gridSize})`;
+        gameGrid.style.height = `calc(100% / ${gridSize})`;
+}
+}
+makeGrid();
+
+function resetGrid(){
+    while (gridContainer.firstChild) {
+        gridContainer.firstChild.remove();
     }
 }
 
+sizeBtn.addEventListener("click", function(){
+    gridSize = prompt("Choose grid size per side (max 100)");
+    resetGrid();
+    makeGrid();
+})
